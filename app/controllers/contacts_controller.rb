6e606b37d21b40d1e385774a6649ad03e157cb0d
野田実 <n0dam1@nodaminorunoMacBook-Air.local>
2017-03-13 01:48:00 +0900

    dive11 exercise

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,24 +6,25 @@ class ContactsController < ApplicationController
       @contact = Contact.new
     end
   end
-  
+
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
       redirect_to root_path, notice: 'お問い合わせが完了しました！'
+      ContactMailer.sendmail_contact(@contact).deliver
     else
       render 'new'
     end
   end
-  
+
   def confirm
     @contact = Contact.new(contact_params)
     render :new if @contact.invalid?
   end
-  
+
   private
     def contact_params
-      params.require(:contact).permit(:name, :email, :content)  
+      params.require(:contact).permit(:name, :email, :content)
     end
-  
+
 end
