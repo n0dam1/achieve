36afe644c2e04b9627f488d9acde07faa693a42a
野田実 <n0dam1@nodaminorunoMacBook-Air.local>
2017-04-29 20:06:58 +0900

    dive15 exam

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,20 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました'}
-        format.js {render :index }
+        format.js { render :index }
       else
         format.html { render :new }
       end
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+
+    @comment.destroy
+    respond_to do |format|
+      format.js
+      format.html{ render :new }
     end
   end
 
